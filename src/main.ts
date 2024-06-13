1078b3ff9b7dd58b27008f6885be512c1d519b2c
Edgar <edgarmihranyan@ingoarmenia.am>
2024-06-13 15:41:47 +0400

    Remove other code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { CORS, PORT } from './config/config';
 import { getSwaggerConfig } from './core/core.utils';
-import { AllExceptionsFilter } from './errors/http-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,10 +21,6 @@ async function bootstrap() {
     optionsSuccessStatus: 200,
     maxAge: -1,
   });
-
-  const httpAdapterHost = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
-  app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = getSwaggerConfig();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
